820f02d116a924505618c3713d719dfc78aafb6b
pavelvasilev <vasilievpavel4@gmail.com>
2025-02-11 11:26:59 +0500

    init for start training on gpu

--- week03_fast_pipelines/homework/task1/train.py
+++ week03_fast_pipelines/homework/task1/train.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from tqdm.auto import tqdm
-
+import wandb
 from unet import Unet
 
 from dataset import get_train_data
@@ -29,10 +29,17 @@ def train_epoch(
         accuracy = ((outputs > 0.5) == labels).float().mean()
 
         pbar.set_description(f"Loss: {round(loss.item(), 4)} " f"Accuracy: {round(accuracy.item() * 100, 4)}")
+        wandb.log(
+            {
+                'Loss': loss,
+                'Accuracy': accuracy
+            }
+        )
 
 
 def train():
-    device = torch.device("cuda:0")
+    wandb.init(project='Loss scaling')
+    device = torch.device("cpu")
     model = Unet().to(device)
     criterion = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
@@ -42,3 +49,5 @@ def train():
     num_epochs = 5
     for epoch in range(0, num_epochs):
         train_epoch(train_loader, model, criterion, optimizer, device=device)
+
+    wandb.finish()
