10f7557773c237a4018410ab479888fc2aa25414
pavelvasilev <vasilievpavel4@gmail.com>
2025-02-15 20:36:42 +0500

    a part of task2

--- week03_fast_pipelines/homework/task2/dataset.py
+++ week03_fast_pipelines/homework/task2/dataset.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Dict
 from enum import Enum
 import torch
 from torch.utils.data.dataset import Dataset
@@ -7,9 +7,12 @@ from transformers import AutoTokenizer
 from torch import nn
 from torch.utils.data import DataLoader
 import transformer
+from collections import Counter
 
 
 MAX_LENGTH = 640
+PAD = 0
+UNK = 1
 
 class DataMode(Enum):
     BRAIN = 1
@@ -19,10 +22,38 @@ class DataMode(Enum):
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
+
+def yield_tokens(data_iter):
+    for text, label in data_iter:
+        yield tokenizer(text)
+
+
 def load_wikitext_data(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         lines = [line.strip() for line in f.readlines() if line.strip()]
     return lines
+
+
+def get_vocab(dataset, tokenizer) -> Dict:
+    counter = Counter()
+    for line in dataset:
+        counter.update(tokenizer(line)["input_ids"])
+
+    vocab = {token: idx + 2 for idx, (token, _) in enumerate(counter.most_common())}
+    # 2, because PAD = 0, UNK = 1
+    return vocab
+
+
+wiki_lines = load_wikitext_data('wikitext-103-raw-v1/test-00000-of-00001.txt')
+vocab = get_vocab(wiki_lines, tokenizer)
+
+
+def get_tgt(encoded_input, pad_token_id):
+    tgt = torch.roll(encoded_input, -1)
+    tgt[-1] = pad_token_id
+    tgt = tgt.to(torch.int)
+    return tgt
+
 
 class BrainDataset(Dataset):
     def __init__(self, data_path: str, max_length: int = MAX_LENGTH):
@@ -41,25 +72,44 @@ class BrainDataset(Dataset):
             return_tensors="pt",
         )
         # print(encoded)
-        return encoded["input_ids"].squeeze(0), transformer.generate_square_subsequent_mask(self.max_length)
-        #
-        # transformer.generate_square_subsequent_mask(self.max_length)
+        src = encoded["input_ids"].squeeze(0).to(torch.int)
+        tgt = torch.roll(src, -1)
+        tgt[-1] = tokenizer.pad_token_id
+        tgt = tgt.to(torch.int)
+        # raise Exception((src, tgt))
+        return src, get_tgt(src, tokenizer.pad_token_id)
 
 
 class BigBrainDataset(Dataset):
     def __init__(self, data_path: str, max_length: int = MAX_LENGTH):
-        pass
+        self.data = load_wikitext_data(data_path)
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
 
     def __getitem__(self, idx: int):
-        pass
+        src = tokenizer(
+            self.data[idx],
+            return_tensors="pt"
+        )["input_ids"].squeeze(0).to(torch.int)
+        return src, get_tgt(src, tokenizer.pad_token_id)
 
 
 class UltraBigBrainDataset(Dataset):
-    def __init__(self, data_path: str, max_length: int = MAX_LENGTH, n_bins: int = 1):
-        pass
+    def __init__(self, data_path: str, max_length: int = MAX_LENGTH, n_bins: int = -1):
+        self.data = load_wikitext_data(data_path)
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
 
     def __getitem__(self, idx: int):
-        pass
+        src = tokenizer(
+            self.data[idx],
+            return_tensors="pt"
+        )["input_ids"].squeeze(0).to(torch.int)
+        return src, get_tgt(src, tokenizer.pad_token_id)
 
 
 class UltraDuperBigBrainDataset(IterableDataset):
@@ -70,25 +120,41 @@ class UltraDuperBigBrainDataset(IterableDataset):
         pass
 
 
-def collate_fn(
-    batch: list[tuple[str, torch.Tensor]], max_length: Optional[int] = MAX_LENGTH
-) -> tuple[torch.Tensor, torch.Tensor]:
-    """
-    Pad each sequence of the incoming sequences list
-    :param batch: a list of the objects received from the dataset by __getitem__
-    :param max_length: maximum sequence length to pad to (for "Brain" approach only)
-    :return: tuple of padded sequences and corresponding training targets
-    """
-    pass
+def collate_fn(batch) -> tuple[torch.Tensor, torch.Tensor]:
+    src_list = []
+    tgt_list = []
+    for src, tgt in batch:
+        processed_src = [vocab.get(token, UNK) for token in src]
+        src_list.append(torch.tensor(processed_src, dtype=torch.int64))
+        processed_tgt = [vocab.get(token, UNK) for token in tgt]
+        tgt_list.append(torch.tensor(processed_tgt, dtype=torch.int64))
+    src_list = nn.utils.rnn.pad_sequence(src_list, batch_first=True, padding_value=PAD)
+    tgt_list = nn.utils.rnn.pad_sequence(src_list, batch_first=True, padding_value=PAD)
+    return tgt_list, src_list
 
 
 class UltraBigBrainBatchSampler(Sampler):
-
-    def __init__(self, batch_size: int, max_length: Optional[int] = MAX_LENGTH):
-        pass
+    def __init__(self, data: torch.Tensor, batch_size: int, max_length: Optional[int] = MAX_LENGTH):
+        self.batch_size = batch_size
+        self.length = len(data)
+        self.len_to_idx = {}
+        for i, (src, tgt) in enumerate(data):
+            length = src.shape[0]
+            if length not in self.len_to_idx:
+                self.len_to_idx[length] = set()
+            self.len_to_idx[length].add(i)
+        self.random_len = next(iter(self.len_to_idx.keys()))
 
     def __len__(self):
-        pass
+        return (self.length + self.batch_size - 1) // self.batch_size
 
     def __iter__(self):
-        pass
+        while len(self.len_to_idx.keys()):
+            if self.random_len not in self.len_to_idx:
+                self.random_len = next(iter(self.len_to_idx.keys()))
+            indices = self.len_to_idx[self.random_len]
+            idx = next(iter(indices))
+            indices.remove(idx)
+            if len(indices) == 0:
+                del self.len_to_idx[self.random_len]
+            yield idx
--- week03_fast_pipelines/homework/task2/run_epoch.py
+++ week03_fast_pipelines/homework/task2/run_epoch.py
@@ -1,30 +1,38 @@
 import transformer
-from dataset import BrainDataset, DataMode
+from dataset import BrainDataset, BigBrainDataset, UltraBigBrainDataset, DataMode, collate_fn, UltraBigBrainBatchSampler
 from torch.utils.data import DataLoader
 import torch
+from torch import nn
+from tqdm import tqdm
 
-
-def get_gpt2_model() -> torch.nn.Module:
-    ntokens = 50257
-    d_model = 768
-    nhead = 12
-    d_hid = 3072
-    nlayers = 12
-    dropout = 0.1
-    model = transformer.TransformerModel(ntokens, d_model, nhead, d_hid, nlayers, dropout)
-    return model
+def get_gpt2_model():
+    return transformer.TransformerLetsGoooooooooooo()
 
 data_path = 'wikitext-103-raw-v1/test-00000-of-00001.txt'
-
 
 def run_epoch(data_mode: DataMode) -> None:
     model = get_gpt2_model()
     dataloader = None
-    dataset = BrainDataset(data_path)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    batch_size = 32
+    shuffle = True
+    print(type(data_mode))
+    if data_mode == DataMode.BRAIN:
+        print('BRAIN BRO')
+        dataset = BrainDataset(data_path)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    elif data_mode == DataMode.BIG_BRAIN:
+        print('BIG BRAIN BRUUH')
+        dataset = BigBrainDataset(data_path)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+    elif data_mode == DataMode.ULTRA_BIG_BRAIN:
+        print('ULTRA BIG BRAIN BRUUH')
+        dataset = BigBrainDataset(data_path)
+        dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn, sampler=UltraBigBrainBatchSampler(dataset, batch_size))
+    elif data_mode == DataMode.ULTRA_DUPER_BIG_BRAIN:
+        print('ULTRA DUPER BIG BRAIN BRUUH')
+    else:
+        print(type(data_mode), type(DataMode.BRAIN))
+        exit(0)
 
-    for data in dataset:
-        input = data[0]
-        print('SHAPE=', data[1][..., None].shape)
-        # att_mask = data[1][..., None].repeat(2, input.shape[-1])
-        output = model(input, data[1])
+    for tgt, src in tqdm(dataloader):
+        output = model(tgt, src)
